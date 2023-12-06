36c1ed9faf150c5d1a6e0aae2ac4004b7fc4b8fe
OsborneLinus <linusholm1997@gmail.com>
2023-12-06 12:41:05 +0100

    First commit with Tailwind.config and basic style of html

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'header-picture': "url('/media/header-pic.png')",
+        'hero-picture': "url('/media/hero-pic.jpeg')",
+      },
+    },
+  },
+  plugins: [],
+};
