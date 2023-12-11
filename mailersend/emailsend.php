8d1c42b6090637017954da829205215f84d1f393
OsborneLinus <122179900+OsborneLinus@users.noreply.github.com>
2023-12-11 17:22:24 +0100

    Update emailsend.php
    removed the api-key

--- mailersend/emailsend.php
+++ mailersend/emailsend.php
@@ -5,10 +5,9 @@ use MailerSend\MailerSend;
 use MailerSend\Helpers\Builder\Recipient;
 use MailerSend\Helpers\Builder\EmailParams;
 
-/* $apiKey = 'mlsn.253642605b665b78b0cc775d380ffa1d4139d094a8f7e3f4c9f53da403140605';
- */
 
-$mailersend = new MailerSend(['api_key' => 'mlsn.356a7a7382d0f002758abfcd1201b5a32a50dbd16aa4e91964dc82b58fed53c9']);
+
+$mailersend = new MailerSend(['api_key' => 'key']);
 
 $recipients = [
     new Recipient('admin@theduckside.se', 'Recipient'),
